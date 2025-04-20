d544e35b0ba84bfe9b759daef4f66f0c19f508ca
vectorkim3333 <sh.kim@madup.com>
2025-04-21 02:58:44 +0900

    chore: 메타 데이터 수정

--- app/color-tiles/page.tsx
+++ app/color-tiles/page.tsx
@@ -2,11 +2,12 @@ import { Metadata } from 'next'
 import TileGame from '../components/tile-game'
 
 export const metadata: Metadata = {
-  title: '타일 게임 - 미니게임 모음',
-  description: '같은 색상의 타일을 연결하여 점수를 얻는 게임입니다. 제한 시간 내에 최대한 많은 점수를 획득해보세요!',
+  title: '타일 게임 - 께에임즈',
+  description: '같은 색상의 타일을 연결해 높은 점수를 노리는 전략 퍼즐 게임! 제한 시간 내 최대한 많은 타일을 연결하고, 순발력과 두뇌를 겨뤄보세요.',
+  keywords: ['타일게임', '색상연결', '퍼즐게임', '전략게임', '순발력', '두뇌게임', '무료게임', 'GGGGame', '께에임즈'],
   openGraph: {
-    title: '타일 게임 - 미니게임 모음',
-    description: '같은 색상의 타일을 연결하여 점수를 얻는 게임입니다. 제한 시간 내에 최대한 많은 점수를 획득해보세요!',
+    title: '타일 게임 - 께에임즈',
+    description: '같은 색상의 타일을 연결해 높은 점수를 노리는 전략 퍼즐 게임! 제한 시간 내 최대한 많은 타일을 연결하고, 순발력과 두뇌를 겨뤄보세요.',
     images: ['/images/color-tiles-thumbnail.png']
   }
 } 
